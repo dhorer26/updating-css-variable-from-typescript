dfb68229ab27bedaf360a4061c48eeddbd451fa5
Ravindra Dhore <ravindrad@ansh.com>
2019-11-27 14:46:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firebase-auth-angular-app';
+  constructor() {
+    document.documentElement.style.setProperty('--bg-red', 'red');
+  }
 }
